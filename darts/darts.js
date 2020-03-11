8c2c6197a83c66bb60438ff93408dcd1cb8f12a4
Elias <eliasgarciadev@gmail.com>
2020-03-11 20:38:55 +0100

    Add solution for the Darts exercise

--- darts/darts.js
+++ darts/darts.js
@@ -3,6 +3,30 @@
 // convenience to get you started writing code faster.
 //
 
-export const score = () => {
-  throw new Error("Remove this statement and implement this function");
+function squareValue(value) {
+  return Math.pow(value, 2)
+}
+
+function computeScore(distanceToOrigin) {
+  if (distanceToOrigin <= 1) {
+    return 10;
+  }
+
+  if (distanceToOrigin <= 5) {
+    return 5;
+  }
+
+  if (distanceToOrigin <= 10) {
+    return 1;
+  }
+
+  return 0;
+}
+
+export const score = (x, y) => {
+  const xSquared = squareValue(Math.abs(x));
+  const ySquared = squareValue(Math.abs(y));
+  const distanceToOrigin = Math.sqrt(xSquared + ySquared);
+
+  return computeScore(distanceToOrigin);
 };
