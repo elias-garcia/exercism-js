5e793af9635f790d2edddbd10789c239cfae350b
Elias <eliasgarciadev@gmail.com>
2020-03-11 20:18:51 +0100

    Add solution for the Resistor Color Duo exercise

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -3,6 +3,25 @@
 // convenience to get you started writing code faster.
 //
 
-export const decodedValue = () => {
-  throw new Error("Remove this statement and implement this function");
+const COLORS = {
+  black: 0,
+  brown: 1,
+  red: 2,
+  orange: 3,
+  yellow: 4,
+  green: 5,
+  blue: 6,
+  violet: 7,
+  grey: 8,
+  white: 9,
+};
+
+const MAX_COLORS = 2;
+
+export const decodedValue = (colors) => {
+  const colorCode = colors
+    .slice(0, MAX_COLORS)
+    .reduce((total, color) => (total * 10) + (COLORS[color]), 0);
+
+  return colorCode;
 };
